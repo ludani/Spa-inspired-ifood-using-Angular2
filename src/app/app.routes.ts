d3e37d8ae0774052db00a44dd38370e45527d05c
ludani <ldanivieira@gmail.com>
2021-03-10 22:08:58 -0400

    corrigindo error e colocando patch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,18 @@ import { HomeComponent } from 'app/home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantsDetailComponent } from './restaurants-detail/restaurants-detail.component';
 import { AboutComponent } from 'app/about/about.component';
+import { MenuComponent } from './restaurants-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurants-detail/reviews/reviews.component';
 
 // CRIANDO ROTAS
 export const ROUTES: Routes = [
     {path:'', component: HomeComponent},
     {path: 'restaurants', component: RestaurantsComponent},
-    {path:'restaurants/:id', component: RestaurantsDetailComponent},
+    {path:'restaurants/:id', component: RestaurantsDetailComponent,
         children: [
-            {}
-            {}
-        ]
+            {path: '', redirectTo: 'menu', pathMatch:'full'},
+            {path: 'menu', component: MenuComponent},
+            {path: 'reviews', component: ReviewsComponent}
+        ]},
     {path:'about', component: AboutComponent},
 ]
