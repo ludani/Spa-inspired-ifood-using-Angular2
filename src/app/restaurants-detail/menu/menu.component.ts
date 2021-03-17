5faad87d104356cd4a3158601f21b7b774488471
ludani <ldanivieira@gmail.com>
2021-03-16 23:56:25 -0400

    implement menu-tem sucess

--- src/app/restaurants-detail/menu/menu.component.ts
+++ src/app/restaurants-detail/menu/menu.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
+import { RestaurantsServices } from '../../restaurants/restaurants.service'
+
+import { Observable } from 'rxjs/Observable'
+import { MenuItem } from '../menu-item/menu-item.model';
 
 @Component({
   selector: 'mt-menu',
@@ -6,9 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  menu: Observable<MenuItem[]>
+
+  constructor(private restaurantsServices: RestaurantsServices,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.menu = this.restaurantsServices
+    .menuOfRestaurant(this.route.parent.snapshot.params['id'])
   }
 
+  addMenuItem(item: MenuItem) {
+    console.log(item)
+  }
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs/Observable'
 import {ErrorHandler} from '../app.error-handler'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
+import { MenuItem } from 'app/restaurants-detail/menu-item/menu-item.model'
 
 // Se foi importar o http precisa colocar esse injectable
 @Injectable()
@@ -27,6 +28,12 @@ export class RestaurantsServices {
 
     reviewsOfRestaurant(id: string): Observable<any>{
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
